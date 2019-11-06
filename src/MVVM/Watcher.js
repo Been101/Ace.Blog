33ea18e329de6a285212aff94fd119574623be08
been <1043609130@qq.com>
2019-11-06 23:04:52 +0800

    blog page

--- src/MVVM/Watcher.js
+++
@@ -1,9 +0,0 @@
-import Dep from './Dep'
-export default class Watcher{
-    constructor() {
-        Dep.target = this // hack 操作，当Watcher 实例时把它加入subs
-    }
-    update() {
-        console.log('属性更新了')
-    }
-}
